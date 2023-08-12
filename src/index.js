ff2fd3ac0e09cfe440818aa03df3c67474e9542e
andsnk <127045868+andsnk@users.noreply.github.com>
2023-08-12 23:24:25 +0300

    add js and style code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,91 @@
-import Notiflix from 'notiflix';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from 'simplelightbox';
-import { fetchImages } from './js/api';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import { getPhoto } from './js/api';
 
-const galleryImages = document.querySelector('.gallery-js');
-const loadMore = document.querySelector('.load-more-btn');
-const searchForm = document.querySelector('.search-form-js');
+const form = document.querySelector('.search-form');
+const loadMore = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', submitForm);
+loadMore.addEventListener('click', onLoadMore);
+
+let page = 1;
+let searchQuery = '';
+let totalPages = 0;
+const lightbox = new SimpleLightbox('.gallery a');
+
+function submitForm(evt) {
+  evt.preventDefault();
+  page = 1;
+  gallery.innerHTML = '';
+  loadMore.classList.add('is-hidden');
+  searchQuery = evt.currentTarget.elements.searchQuery.value;
+  if (searchQuery) {
+    getPhoto(searchQuery, page)
+      .then(data => {
+        if (data.hits.length !== 0) {
+          createMarkup(data);
+          loadMore.classList.remove('is-hidden');
+          totalPages = Math.ceil(data.totalHits / 40);
+          console.log(totalPages);
+          Notiflix.Notify.success(`Success, found ${data.totalHits} images`);
+          form.reset();
+        } else
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+      })
+      .catch(error => Notiflix.Notify.failure(error.message));
+  } else Notiflix.Notify.info('Please fill in the search field');
+}
+
+function createMarkup(data) {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<a href="${largeImageURL}" class="photo-card">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300px" height="200px" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>${downloads}
+        </p>
+      </div>
+    </a>`
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+function onLoadMore() {
+  page += 1;
+  getPhoto(searchQuery, page)
+    .then(data => {
+      createMarkup(data);
+      if (page >= totalPages) {
+        loadMore.classList.add('is-hidden');
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => Notiflix.Notify.failure(error.message));
+}
--- src/js/api.js
+++ src/js/api.js
@@ -2,22 +2,32 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38682723-ccb8378626c039a4163c61aff';
-const PAGE = 1;
 
-export async function fetchImages(query, page, perPage) {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: `${query}`,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: perPage,
-    page: page,
-  });
-  return await axios.get(`${BASE_URL}?${searchParams}`).then(response => {
-    if (response.status !== 200) {
-      throw new Error(response.statusText);
-    }
-    return response.data;
-  });
-}
+export const getPhoto = async (query, page) => {
+  try {
+    const { data } = await axios(
+      `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// export async function fetchImages(query, page, perPage) {
+//   const searchParams = new URLSearchParams({
+//     key: API_KEY,
+//     q: `${query}`,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     per_page: perPage,
+//     page: page,
+//   });
+//   return await axios.get(`${BASE_URL}?${searchParams}`).then(response => {
+//     if (response.status !== 200) {
+//       throw new Error(response.statusText);
+//     }
+//     return response.data;
+//   });
+// }
